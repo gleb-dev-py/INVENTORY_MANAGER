403330f45e7651b93513010c7d3cd6815f1cae41
Gleb <155180606+gleb-dev-py@users.noreply.github.com>
2026-08-24 20:32:00 +0300

    Delete django_site directory

--- django_site/inventory_control/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.base_version, name='base'),
-    path('test', views.dark_version, name='dark')
-]
--- django_site/inventory_control/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-def base_version(request):
-    return render(request, 'register.html')
-
-
-def dark_version(request):
-    return render(request, 'dark.html')
